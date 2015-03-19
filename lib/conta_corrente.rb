c8d8b1105c06a86e4ed59f8befc7653a34d92a0c
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-19 17:41:54 -0300

    applying tax on deposit

--- lib/conta_corrente.rb
+++ lib/conta_corrente.rb
@@ -1,5 +1,12 @@
 class ContaCorrente < Conta
+  TAXA_DEPOSITO = 0.10
+
   def initialize(args)
     super(args)
+  end
+
+  def depositar(valor)
+    valor -= TAXA_DEPOSITO
+    super(valor)
   end
 end
