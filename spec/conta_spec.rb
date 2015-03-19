37219606a409700541c189bf3af435d309ec9839
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-03-19 13:23:50 -0300

    add test, sacar method

--- spec/conta_spec.rb
+++ spec/conta_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe Conta do
   it "deveria conseguir criar um objeto do tipo Conta" do
-    numero = "2303-2"
-    nome = "Jose da Silva"
-    saldo = 1000.10
-    limite = 500
-    conta = Conta.new(numero: numero, nome: nome, saldo: saldo, limite: limite)
+    conta = cria_conta
     expect(conta.numero).to eq numero
     expect(conta.nome).to eq nome
     expect(conta.limite).to eq limite
     expect(conta.saldo).to eq saldo
+  end
+
+  it "deveria ter um novo saldo quando houver um saque" do
+    conta = cria_conta
+    saldo_anterior = conta.saldo
+    saque = 200
+    conta.sacar(saque)
+    expect(conta.saldo).eq to saldo_anterior - saque
+  end
+
+  def cria_conta(numero="2303-2", nome="Jose da Silva", saldo=1000.10, limite=500)
+    Conta.new(numero: numero, nome: nome, saldo: saldo, limite: limite)
   end
 end
