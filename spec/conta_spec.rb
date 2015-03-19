9c370042b2274914f89efd57e6c010ea485c045c
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-03-19 13:09:39 -0300

    add conta spec v1

---
+++ spec/conta_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Conta do
+  it "deveria conseguir criar um objeto do tipo Conta" do
+    numero = "2303-2"
+    nome = "Jose da Silva"
+    saldo = 1000.10
+    limite = 500
+    conta = Conta.new(numero: numero, nome: nome, saldo: saldo, limite: limite)
+    expect(conta.numero).to eq numero
+    expect(conta.nome).to eq nome
+    expect(conta.limite).to eq limite
+    expect(conta.saldo).to eq saldo
+  end
+end
