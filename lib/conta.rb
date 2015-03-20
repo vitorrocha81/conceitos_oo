68a59639361ab158b11e8683c76c39d1ee41ac5c
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-20 20:13:39 -0300

    change nome to titular

--- lib/conta.rb
+++ lib/conta.rb
@@ -1,12 +1,12 @@
 class Conta
-attr_accessor :numero, :nome, :saldo, :limite
+attr_accessor :numero, :titular, :saldo, :limite
  def new
-  conta = Conta.new 
+  conta = Conta.new
  end
 
  def initialize (conta_params)
   @numero = conta_params[:numero]
-  @nome = conta_params[:nome]
+  @titular = conta_params[:titular]
   @saldo = conta_params[:saldo]
   @limite = conta_params[:limite]
  end
@@ -28,6 +28,32 @@ attr_accessor :numero, :nome, :saldo, :limite
  end
 
  def ==(conta)
-  @numero == conta.numero && @nome == conta.nome && @saldo == conta.saldo && @limite == conta.limite  
+  @numero == conta.numero && @titular == conta.titular && @saldo == conta.saldo && @limite == conta.limite  
  end
+
+ def transfere(conta_destino,valor_transferencia)
+    if transferencia_ok(valor_transferencia)
+    @saldo -= valor_transferencia
+    conta_destino.saldo += valor_transferencia
+    else
+    false
+    end
+  end
+
+  def transferencia_ok(valor_transferencia)
+    if @saldo - valor_transferencia >= 0
+    true
+    else
+    false
+    end
+  end
 end
+
+    #conta_origem = cria_conta
+    #saldo_anterior_origem = conta_origem.saldo
+    #conta_destino = cria_conta("3432-2", "Zenildo Bispo", 4000.54)
+    #saldo_anterior_destino = conta_destino.saldo
+    #valor_transferencia = 560.40
+    #conta_origem.transfere(conta_destino, valor_transferencia)
+    #expect(conta_origem.saldo).to eq(saldo_anterior_origem - valor_transferencia)
+    #expect(conta_destino.saldo).to eq(saldo_anterior_destino + valor_transferencia)
--- spec/conta_spec.rb
+++ spec/conta_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Conta do
   it "deveria conseguir criar um objeto do tipo Conta" do
     numero = "2303-2"
-    nome = "Jose da Silva"
+    titular = "Jose da Silva"
     saldo = 1000.10
     limite = 500
     conta = cria_conta
     expect(conta.numero).to eq numero
-    expect(conta.nome).to eq nome
+    expect(conta.titular).to eq titular
     expect(conta.limite).to eq limite
     expect(conta.saldo).to eq saldo
   end
@@ -69,7 +69,6 @@ describe Conta do
   end
 
   it "deveria ser false quando não for possivel transferir" do
-    pending
     conta_origem = cria_conta
     conta_destino = cria_conta("3432-2", "Zenildo Bispo", 4000.54)
     valor_transferencia = 1560.40
@@ -78,15 +77,14 @@ describe Conta do
   end
 
   it "titular deveria ser um Cliente" do
-    pending
     # Bonus
-    # altere conta para ter titular ao inves de nome
+    # altere conta para ter titular ao inves de titular
     # altere tambem os testes para que passem nesse nova versão
     # titular deveria ser um objeto do tipo Cliente
     expect(conta.titular).to be_a(Cliente)
   end
 
-  def cria_conta(numero="2303-2", nome="Jose da Silva", saldo=1000.10, limite=500)
-    Conta.new(numero: numero, nome: nome, saldo: saldo, limite: limite)
+  def cria_conta(numero="2303-2", titular="Jose da Silva", saldo=1000.10, limite=500)
+    Conta.new(numero: numero, titular: titular, saldo: saldo, limite: limite)
   end
 end
